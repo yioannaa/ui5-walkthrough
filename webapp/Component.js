316cf947ac96f02ad0363c91f8fd85c0d8164f58
Joanna Krzyzanowska <joanna.krzyzanowska@o2.pl>
2022-06-24 09:11:17 +0000

    step 31: Routing and Navigation

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,35 +1,42 @@
-sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "./controller/HelloDialog"
-], function (UIComponent, JSONModel, HelloDialog) {
-    "use strict";
-    return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
-        metadata: {
-            manifest: "json"
-        },
-        init: function () {
-
-            UIComponent.prototype.init.apply(this, arguments);
-
-            var oData = {
-                recipient: {
-                    name: "UI5"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.setModel(oModel);
-
-            this._helloDialog = new HelloDialog(this.getRootControl());
-        },
-
-        exit: function () {
-            this._helloDialog.destroy();
-            delete this._helloDialog;
-        },
-        
-        openHelloDialog : function (){
-            this._helloDialog.open();
-        }
+  sap.ui.define([
+        "sap/ui/core/UIComponent",
+        "sap/ui/model/json/JSONModel",
+        "./controller/HelloDialog"
+    ], function (UIComponent, JSONModel, HelloDialog) {
+        "use strict";
+        return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+            metadata: {
+                manifest: "json"
+            },
+            init: function () {
+    
+                UIComponent.prototype.init.apply(this, arguments);
+    
+                var oData = {
+                    recipient: {
+                        name: "UI5"
+                    }
+                };
+                var oModel = new JSONModel(oData);
+                this.setModel(oModel);
+    
+                this._helloDialog = new HelloDialog(this.getRootControl());
+            
+    
+                //create the views based on the url
+                this.getRouter().initialize();
+    
+            },
+    
+            exit: function () {
+                this._helloDialog.destroy();
+                delete this._helloDialog;
+            },
+            
+            openHelloDialog : function (){
+                this._helloDialog.open();
+            }
+        });
     });
-});
+    
+    
